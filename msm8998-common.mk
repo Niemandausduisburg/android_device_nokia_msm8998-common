a0aef4114a4f45828666b56601b3340bc31c72eb
mikeNG <mikeioannina@gmail.com>
2025-04-06 14:44:18 +0000

    msm8998-common: Build charger_res_images
    * Fix offline charging animation not showing
    
    Change-Id: Ib35197afe78b12b840b59c9e3b34e46bc559f0ff
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -77,8 +77,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
 
-# Charger
 PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger_res_images_vendor \
     libsuspend
 
 # ConfigStore
