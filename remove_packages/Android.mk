Niemandausduisburg/android_device_xiaomi_sm8250-common

msm8998-common: Remove unneeded Gapps Packages
They can easy installed from Google Play Store

remove_packages/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := CalculatorGoogle DeskClockGoogle Gmail2 FilesGoogle GoogleFeedback Chrome64 Drive Photos YouTube Maps
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)

Niemandausduisburg/android_device_nokia_msm8998-common

remove_packages/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackagesNokia
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := CalculatorGoogle DeskClockGoogle Gmail2 FilesGoogle GoogleFeedback Chrome64 Drive Photos YouTube Maps
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)